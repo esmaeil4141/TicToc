e92a763decc4e0b53eb2506a28c9f9210434299f
mojtaba bahrami <mbahram721@gmail.com>
2016-08-15 19:30:49 +0430

    change DatePicker to a Persian one

--- app/src/main/java/ir/sharif/random/tictoc/view/FragmentCreateTask.java
+++ app/src/main/java/ir/sharif/random/tictoc/view/FragmentCreateTask.java
@@ -1,25 +1,19 @@
 package ir.sharif.random.tictoc.view;
 
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
+
 import android.app.Fragment;
-import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TimePicker;
 
-import java.text.SimpleDateFormat;
+import com.mohamadamin.persianmaterialdatetimepicker.date.DatePickerDialog;
+import com.mohamadamin.persianmaterialdatetimepicker.utils.PersianCalendar;
+
 import java.util.Calendar;
-import java.util.Date;
 
 import ir.sharif.random.tictoc.R;
 import ir.sharif.random.tictoc.model.Task;
@@ -27,21 +21,30 @@ import ir.sharif.random.tictoc.model.Task;
 /**
  * Created by Mojtaba on 8/14/2016.
  */
-public class FragmentCreateTask extends Fragment {
+public class FragmentCreateTask extends Fragment implements DatePickerDialog.OnDateSetListener {
     private static Task task;
     private CallBack callback;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_task_create,container,false);
+        View root = inflater.inflate(R.layout.fragment_task_create, container, false);
         Button button = (Button) root.findViewById(R.id.buttonTimePick);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                DialogFragment newFragment = new DatePickerFragment();
-                newFragment.show(getFragmentManager(), "datePicker");
+//                DialogFragment newFragment = new DatePickerFragment();
+//                newFragment.show(getFragmentManager(), "datePicker");
+                PersianCalendar persianCalendar = new PersianCalendar();
+
+                DatePickerDialog datePickerDialog = DatePickerDialog.newInstance(
+                        FragmentCreateTask.this,
+                        persianCalendar.getPersianYear(),
+                        persianCalendar.getPersianMonth(),
+                        persianCalendar.getPersianDay()
+                );
+                datePickerDialog.show(getFragmentManager(), "Datepickerdialog");
             }
         });
         button = (Button) root.findViewById(R.id.addTask);
@@ -54,48 +57,13 @@ public class FragmentCreateTask extends Fragment {
 
         return root;
     }
-    public static class TimePickerFragment extends DialogFragment
-            implements TimePickerDialog.OnTimeSetListener {
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
-        }
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, monthOfYear, dayOfMonth);
     }
 
-    public static class DatePickerFragment extends DialogFragment
-            implements DatePickerDialog.OnDateSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current date as the default date in the picker
-            final Calendar c = Calendar.getInstance();
-            int year = c.get(Calendar.YEAR);
-            int month = c.get(Calendar.MONTH);
-            int day = c.get(Calendar.DAY_OF_MONTH);
-
-            // Create a new instance of DatePickerDialog and return it
-            return new DatePickerDialog(getActivity(), this, year, month, day);
-        }
-
-        public void onDateSet(DatePicker view, int year, int month, int day) {
-            // Do something with the date chosen by the user
-            Calendar c = Calendar.getInstance();
-            c.set(year,month,day);
-            task.setDate(new SimpleDateFormat("yyyy-MM-dd").format(c.getTime()));
-        }
-    }
 
     @Override
     public void onAttach(Context context) {
